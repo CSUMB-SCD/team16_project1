cdf473541a60c55ec0a15b998702cbf29ac17366
rami9284 <judramirez@csumb.edu>
2018-11-01 18:57:40 -0700

    Added springboot service

---
+++ otterBuyService/otterBuyService/src/main/java/edu/csumb/cst438/otterBuyService/OtterBuyServiceApplication.java
@@ -0,0 +1,12 @@
+package edu.csumb.cst438.otterBuyService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtterBuyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtterBuyServiceApplication.class, args);
+	}
+}
---
+++ otterBuyService/otterBuyService/src/test/java/edu/csumb/cst438/otterBuyService/OtterBuyServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.csumb.cst438.otterBuyService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OtterBuyServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
