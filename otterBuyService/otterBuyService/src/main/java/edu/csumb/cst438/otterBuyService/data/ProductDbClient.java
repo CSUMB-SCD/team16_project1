35b57ad43440d75aa6a2eed42657f23a3cfefa7c
rami9284 <judramirez@csumb.edu>
2018-11-02 15:33:09 -0700

    Springboot updating

---
+++ otterBuyService/otterBuyService/src/main/java/edu/csumb/cst438/otterBuyService/data/ProductDbClient.java
@@ -0,0 +1,14 @@
+package edu.csumb.cst438.otterbuyservice.data;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import edu.csumb.cst438.otterbuyservice.api.entities.Product;
+
+public class ProductDbClient{
+
+    public List<Product> getproductData(){
+        return null;
+    }
+}
